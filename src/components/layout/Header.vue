a2e7c974f303b5524fdec47a6778a29963ebb7f3
hand-Sum MUSE <egbouchenna001@gmail.com>
2019-09-30 21:51:23 +0100

    Feature - Create Header component

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,87 @@
+<template>
+  <header>
+    <div class="overlay">
+      <div class="container-fluid">
+        <div class="row">
+          <div id="site-logo" class="m-5">
+            <a href="/">
+              <img :src="logo" alt="Star Wars Logo" class />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row text-white" v-if="genericHeader">
+          <div class="col-12 text-center mb-5" id="header-title">
+            <h1 class="text-uppercase">
+              <img id="starwars-image-title" :src="logo" alt="Star Wars" class="pb-3" />
+              Directory
+            </h1>
+            <p id="header-title-tagline">{{ tagline }}</p>
+          </div>
+          <div class="col-md-8 mx-auto">
+            <SearchForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import logo from "../../assets/images/logo.png";
+
+import SearchForm from "../SearchForm.vue";
+export default {
+  name: "Header",
+  data() {
+    return {
+      tagline: "Your one stop shop for all things STAR WARS!!!",
+      logo: logo,
+      genericHeader: true
+    };
+  },
+  components: {
+    SearchForm
+  }
+};
+</script> 
+
+<style scoped>
+header {
+  background-image: url("../../assets/images/vortex.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 65% 100%;
+  height: 700px;
+}
+
+h1 {
+  font-size: 4rem;
+  font-weight: 900;
+}
+
+.overlay {
+  background-color: rgba(0, 0, 0, 0.4);
+  height: 100%;
+}
+
+#site-logo {
+  height: 50px;
+}
+#site-logo img {
+  height: 100%;
+}
+#starwars-image-title {
+  height: 70px;
+}
+#header-title-tagline {
+  font-size: 1.75rem;
+}
+
+@media only screen and (min-width: 960px) {
+  #header-title {
+    margin-top: 200px;
+  }
+}
+</style>
