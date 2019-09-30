53bb71168c7f57d8a204297b21898e1eae880f8c
hand-Sum MUSE <egbouchenna001@gmail.com>
2019-09-30 21:35:36 +0100

    Feature - Create search form component

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-form @submit="onSearch">
+    <div class="input-group mb-2 mr-sm-2">
+      <b-form-input
+        type="text"
+        v-model="formInput"
+        class="form-control form-control-lg text-center font-italic"
+        placeholder="Looking for something?"
+      ></b-form-input>
+      <div class="input-group-append">
+        <b-button
+          type="submit"
+          variant="primary"
+          class="btn btn-primary pl-sm-4 pr-sm-4 pr-3 pl-3"
+        >{{ searchButtonValue }}</b-button>
+      </div>
+    </div>
+  </b-form>
+</template>
+
+<script>
+export default {
+  name: "SearchForm",
+  data() {
+    return {
+      formInput: "",
+      submitButton: "submit",
+      searchButtonValue: "Search"
+    };
+  },
+  methods: {
+    onSearch(evt) {
+      evt.preventDefault();
+    }
+  }
+};
+</script>
+
+<style>
+</style>
