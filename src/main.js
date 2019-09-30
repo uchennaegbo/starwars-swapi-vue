b2a19042cf06efbd7d8d742b37ddd03ba50b4f6d
hand-Sum MUSE <egbouchenna001@gmail.com>
2019-09-30 21:28:57 +0100

    Configured app to use bootstrap vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import App from './App.vue';
+import './assets/css/cosmo.bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
