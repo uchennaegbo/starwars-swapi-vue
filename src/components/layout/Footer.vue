0f55359f470bf1e74e4ade046db354fa2b57ff63
hand-Sum MUSE <egbouchenna001@gmail.com>
2019-10-01 01:59:06 +0100

    Feature - Create Footer component

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer class="bg-dark text-white">
+    <div class="container pt-3 pb-3">
+      <div class="row text-center">
+        <div class="col-12 small">
+          &copy; {{ footerYear() }}
+          <strong>{{ siteName }}</strong>. All rights
+          reserved.
+          <br />Site designed by
+          <a
+            :href="authorURL"
+            target="_blank"
+            class="text-decoration-none text-white author-credits font-weight-bold"
+            rel="noopener noreferrer"
+          >{{ authorName }}</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      siteName: "StarWars Directory",
+      authorName: "Kingsley Okeke",
+      authorURL: "https://kingsleyokeke.com"
+    };
+  },
+  methods: {
+    footerYear() {
+      const currentYear = new Date().getFullYear();
+      return currentYear <= "2019" ? "2019" : `2019 - ${currentYear}`;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.author-credits:hover {
+  color: gold !important;
+}
+</style>
