18e93a99f6bc2d49b8c1266e2f17a565796c388e
hand-Sum MUSE <egbouchenna001@gmail.com>
2019-10-02 00:51:32 +0100

    StarshipCard now uses starship name as image alt

--- src/components/StarshipCard.vue
+++ src/components/StarshipCard.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="col-sm-6 col-md-6 col-lg-4 mb-5">
     <div class="card bg-grey card-elevated">
-      <img :src="require(`@/assets/images/starship-${index}.jpg`)" class="card-img-top" alt="..." />
+      <img
+        :src="require(`@/assets/images/starship-${index}.jpg`)"
+        class="card-img-top"
+        :alt="`Picture of ${starship.name} starship`"
+      />
       <div class="card-body">
         <h3 class="card-title font-weight-normal">{{starship.name}}</h3>
         <p class="card-text">{{ starship.description }}</p>
