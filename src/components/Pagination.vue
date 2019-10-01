67c7bc217e467028a2ddc08d4685f9b33b625684
hand-Sum MUSE <egbouchenna001@gmail.com>
2019-10-01 10:32:23 +0100

    Feature - Create Pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- Pagination -->
+  <div class="row">
+    <div class="col-4 mx-auto pt-5">
+      <nav aria-label="Page navigation">
+        <ul class="pagination pagination-lg justify-content-center">
+          <li class="page-item">
+            <a class="page-link text-dark" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item" v-for="(link, index) in links" :key="index">
+            <a class="page-link text-dark" :href="link.href">{{link.page}}</a>
+          </li>
+          <li class="page-item">
+            <a class="page-link text-dark" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagination",
+  data() {
+    return {
+      links: [
+        { href: "#", page: "1" },
+        { href: "#", page: "2" },
+        { href: "#", page: "3" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
