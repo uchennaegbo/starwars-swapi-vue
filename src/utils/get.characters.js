2ad43f03c58a61128a0363c76b7b01041a8f5549
hand-Sum MUSE <egbouchenna001@gmail.com>
2019-10-02 02:55:23 +0100

    Modified getCharacter util file to handle queries

--- src/utils/get.characters.js
+++ src/utils/get.characters.js
@@ -1,28 +1,35 @@
 import axios from 'axios';
 
-export default async function() {
-  const results = await axios.get(`https://swapi.co/api/people`);
+export default async function(page = 1) {
+  try {
+    const response = await axios.get(
+      `https://swapi.co/api/people?page=${page}`
+    );
 
-  const characters = results.data.results.map(character => {
-    const slug = character.name.toLowerCase().replace(' ', '-');
-    const gender =
-      character.gender === 'n/a'
-        ? 'Robot'
-        : character.gender.slice(0, 1).toUpperCase() +
-          character.gender.slice(1);
+    const characters = response.data.results.map(character => {
+      const slug = character.name.toLowerCase().replace(/ /g, '-');
+      const gender =
+        character.gender === 'n/a'
+          ? 'Robot'
+          : character.gender.slice(0, 1).toUpperCase() +
+            character.gender.slice(1);
 
-    return {
-      name: character.name,
-      slug: slug,
-      birth_year: character.birth_year,
-      gender: gender,
-      height: character.height,
-      mass: character.mass,
-      hair_color: character.hair_color,
-      skin_color: character.skin_color,
-      eye_color: character.eye_color
-    };
-  });
+      return {
+        name: character.name,
+        slug: slug,
+        birth_year: character.birth_year,
+        gender: gender,
+        height: character.height,
+        mass: character.mass,
+        hair_color: character.hair_color,
+        skin_color: character.skin_color,
+        eye_color: character.eye_color
+      };
+    });
 
-  return characters;
+    return characters;
+  } catch (error) {
+    //eslint-disable-next-line
+    console.error(error);
+  }
 }
