44af64ee0ff5ba22fc3ef544a16e49f1e1f3d718
hand-Sum MUSE <egbouchenna001@gmail.com>
2019-10-02 01:51:39 +0100

    Modified characters page to

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -8,13 +8,12 @@
         <div class="col-lg-5">
           <div class="card-body">
             <h3 class="card-title font-weight-bold">{{ character.name }}</h3>
-            <p class="card-text">
-              {{ character.description }}...
-              <a
-                class="read-more-link text-dark text-decoration-none font-weight-bold"
-                href="#"
-              >{{ readMoreText }}</a>
-            </p>
+            <p class="card-text">Gender: {{ character.gender }}</p>
+            <p class="card-text">Year of Birth: {{ character.birth_year }}</p>
+            <a
+              class="read-more-link text-dark text-decoration-none font-weight-bold mr-2 mb-3"
+              href="#"
+            >{{ readMoreText }}</a>
           </div>
         </div>
       </div>
