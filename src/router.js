5d7e934ac0498e39cbcd9f17927de27435cf388f
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-10-04 16:51:05 +0100

    Routing - Linked planets and character pages.

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Router from 'vue-router';
 import Home from './components/layout/Home.vue';
 import Starships from './components/layout/Starships.vue';
 import Characters from './components/layout/Characters.vue';
+import Planets from './components/layout/Planets.vue';
 
 Vue.use(Router);
 
@@ -21,14 +22,32 @@ export default new Router({
       component: Starships
     },
     {
+      path: '/planets',
+      name: 'planets',
+      component: Planets
+    },
+    {
       path: '/characters',
       name: 'characters',
       component: Characters
-    },{
+    },
+    {
       path: '/starship/:id',
       name: 'starship',
       component: () =>
         import(/* webpackChunkName: "starships" */ './components/views/Starship.vue')
     },
+    {
+      path: '/planet/:id',
+      name: 'planet',
+      component: () =>
+        import(/* webpackChunkName: "starships" */ './components/views/Planet.vue')
+    },
+    {
+      path: '/character/:id',
+      name: 'character',
+      component: () =>
+        import(/* webpackChunkName: "character" */ './components/views/Character.vue')
+    }
   ]
 });
